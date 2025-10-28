ce1de3fe7ec609b8c96917cdc89b05d9a9892a7c
davijannotti <davi29012004@gmail.com>
2025-10-28 19:24:54 -0300

    remove base to correct pathway

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/lp-blog-pulsar-ai/",
-  
+export default defineConfig(({ mode }) => ({  
   server: {
     host: "::",
     port: 8080,
